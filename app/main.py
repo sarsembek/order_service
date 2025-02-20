508aaabac6907c086765fc97abf1c8ed4270bde1
sarsembek <ar_sarsembek@kbtu.kz>
2025-02-21 01:11:44 +0500

    fix: silence error raised by passlib module on bcrypt module attribute "about"

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import bcrypt
+if not hasattr(bcrypt, "__about__"):
+    bcrypt.__about__ = type("dummy", (), {"__version__": "4.2.1"})
+
 from fastapi import FastAPI
 from app.routers.auth import router as auth_router
 
@@ -8,4 +12,3 @@ app.include_router(auth_router)
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-    
